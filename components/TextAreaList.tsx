00ff94852c0b86ab53c4ae8a144859f7bbf1249e
Bogdan Oprea <bogdan.oprea@everymatrix.com>
2024-07-18 00:18:23 +0300

    Progress_Fixed_bugs

--- components/TextAreaList.tsx
+++ components/TextAreaList.tsx
@@ -8,20 +8,33 @@ import React, {
 
 const TextAreaList: React.FC = () => {
   const [text, setText] = useState<string>("1. ");
+  const [focusedLine, setFocusedLine] = useState<number>(1);
   const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   const newText = updateNumbering(text);
+  //   setText(newText);
+  // }, [text]);
+
+  const handleSelectionChange = () => {
     if (textAreaRef.current) {
-      textAreaRef.current.setSelectionRange(text.length, text.length);
+      const textarea = textAreaRef.current;
+      const cursorPosition = textarea.selectionStart;
+      const textBeforeCursor = textarea.value.substring(0, cursorPosition);
+      const lineNumber = textBeforeCursor.split("\n").length;
+      setFocusedLine(lineNumber);
     }
-  }, [text]);
+  };
 
   const updateNumbering = (value: string): string => {
     const lines = value.split("\n");
     return lines
       .map((line, index) => {
         const trimmedLine = line.trim();
-        if (trimmedLine === "" || trimmedLine === `${index + 1}.`) {
+        if (
+          trimmedLine === "" ||
+          (trimmedLine === `${index + 1}.` && index === focusedLine - 1)
+        ) {
           return "";
         }
         const lineContent = line.replace(/^\d+\.\s*/, "");
@@ -32,12 +45,28 @@ const TextAreaList: React.FC = () => {
   };
 
   const handleKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    const lines = text.split("\n");
+    const lastLineNumber = lines.length;
     if (e.key === "Enter") {
       e.preventDefault();
-      const lines = text.split("\n");
-      const currentLineNumber = lines.length;
-      const newLine = `${currentLineNumber + 1}. `;
-      setText(text + "\n" + newLine);
+      if (lines[lastLineNumber - 1] == "" && lastLineNumber <= 1) {
+        lines[0] = "1. ";
+        lines[1] = "2. ";
+        const linesToText = lines.join("\n");
+        setText(linesToText);
+        return;
+      }
+      lines.splice(focusedLine, 0, `${focusedLine + 1}. `);
+      const linesToText = updateNumbering(lines.join("\n"));
+
+      setText(linesToText);
+    }
+    if (e.key === "Backspace") {
+      if (lines[focusedLine - 1] === `${focusedLine}. `) {
+        lines.splice(focusedLine - 1, 1);
+        const linesToText = updateNumbering(lines.join("\n"));
+        setText(linesToText);
+      }
     }
   };
 
@@ -53,6 +82,9 @@ const TextAreaList: React.FC = () => {
       value={text}
       onChange={handleChange}
       onKeyDown={handleKeyDown}
+      onSelect={handleSelectionChange}
+      onKeyUp={handleSelectionChange}
+      onClick={handleSelectionChange}
       rows={10}
       cols={50}
       style={{ whiteSpace: "pre-wrap" }}
