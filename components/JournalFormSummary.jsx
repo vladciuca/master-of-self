edf1ccc3bd673eba719e9f0d196caa99e5159166
Vlad Ciuca <vladccdev@gmail.com>
2024-09-04 16:18:11 +0300

    commented out files that havent been refactored yet

--- components/JournalFormSummary.jsx
+++ components/JournalFormSummary.jsx
@@ -1,49 +1,49 @@
-import data from "@emoji-mart/data";
-import { init } from "emoji-mart";
-import { Button } from "./ui/button";
-import { FaBoltLightning } from "react-icons/fa6";
+// import data from "@emoji-mart/data";
+// import { init } from "emoji-mart";
+// import { Button } from "./ui/button";
+// import { FaBoltLightning } from "react-icons/fa6";
 
-init({ data });
+// init({ data });
 
-const JournalFormSummary = ({
-  gratefulItems,
-  habits,
-  habitWillpower,
-  submitting,
-  handleSubmit,
-}) => {
-  return (
-    <div className="h-full flex flex-col">
-      <div className="flex-grow overflow-y-auto">
-        {Object.entries(habitWillpower).map(([key, value]) => {
-          const habit = habits.find((habit) => habit._id === key);
+// const JournalFormSummary = ({
+//   gratefulItems,
+//   habits,
+//   habitWillpower,
+//   submitting,
+//   handleSubmit,
+// }) => {
+//   return (
+//     <div className="h-full flex flex-col">
+//       <div className="flex-grow overflow-y-auto">
+//         {Object.entries(habitWillpower).map(([key, value]) => {
+//           const habit = habits.find((habit) => habit._id === key);
 
-          return (
-            <div key={key} className="flex items-center justify-center">
-              <em-emoji shortcodes={habit.icon} size="3rem" />
-              <span className="mx-3 text-3xl">+{value}</span>
-              <FaBoltLightning size="1.7rem" />
-            </div>
-          );
-        })}
-        <ol className="list-decimal mx-12 md:mx-6 my-6">
-          {gratefulItems?.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ol>
-      </div>
-      <div className="flex justify-center items-center py-2">
-        <Button
-          type="button"
-          disabled={submitting}
-          className="w-2/3"
-          onClick={handleSubmit}
-        >
-          Submit
-        </Button>
-      </div>
-    </div>
-  );
-};
+//           return (
+//             <div key={key} className="flex items-center justify-center">
+//               <em-emoji shortcodes={habit.icon} size="3rem" />
+//               <span className="mx-3 text-3xl">+{value}</span>
+//               <FaBoltLightning size="1.7rem" />
+//             </div>
+//           );
+//         })}
+//         <ol className="list-decimal mx-12 md:mx-6 my-6">
+//           {gratefulItems?.map((item, index) => (
+//             <li key={index}>{item}</li>
+//           ))}
+//         </ol>
+//       </div>
+//       <div className="flex justify-center items-center py-2">
+//         <Button
+//           type="button"
+//           disabled={submitting}
+//           className="w-2/3"
+//           onClick={handleSubmit}
+//         >
+//           Submit
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default JournalFormSummary;
+// export default JournalFormSummary;
