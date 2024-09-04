822e9dc71c907714cf666bc804afb48841fd6460
Vlad Ciuca <vladccdev@gmail.com>
2024-09-04 16:14:07 +0300

    commented out files that havent been refactored yet

--- components/JournalFormResource.jsx
+++ components/JournalFormResource.jsx
@@ -1,16 +1,16 @@
-import { FaBoltLightning } from "react-icons/fa6";
+// import { FaBoltLightning } from "react-icons/fa6";
 
-const JournalFormResource = ({ resource }) => {
-  return (
-    <div>
-      <div className="flex justify-center items-center">
-        <h1 className="text-6xl font-bold tracking-tight text-center mr-2">
-          {resource}
-        </h1>
-        <FaBoltLightning size={"3rem"} />
-      </div>
-    </div>
-  );
-};
+// const JournalFormResource = ({ resource }) => {
+//   return (
+//     <div>
+//       <div className="flex justify-center items-center">
+//         <h1 className="text-6xl font-bold tracking-tight text-center mr-2">
+//           {resource}
+//         </h1>
+//         <FaBoltLightning size={"3rem"} />
+//       </div>
+//     </div>
+//   );
+// };
 
-export default JournalFormResource;
+// export default JournalFormResource;
