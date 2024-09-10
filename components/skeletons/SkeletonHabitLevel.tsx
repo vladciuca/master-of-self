895fa54afb4b1a1505fd04fda5b4c37cea072eee
Vlad Ciuca <vladccdev@gmail.com>
2024-09-10 18:29:08 +0300

    added skeleton habit level

--- components/skeletons/SkeletonHabitLevel.tsx
+++ components/skeletons/SkeletonHabitLevel.tsx
@@ -8,6 +8,16 @@ const SkeletonHabitLevel = () => {
         <Skeleton className="h-24 w-24 rounded-full mb-10" />
         <Skeleton className="h-8 w-8 rounded-full mb-10" />
       </div>
+      <div className="flex justify-around items-center my-8">
+        <Skeleton className="h-8 w-8 rounded-full mb-10" />
+        <Skeleton className="h-24 w-24 rounded-full mb-10" />
+        <Skeleton className="h-8 w-8 rounded-full mb-10" />
+      </div>
+      <div className="flex justify-around items-center my-8">
+        <Skeleton className="h-8 w-8 rounded-full mb-10" />
+        <Skeleton className="h-24 w-24 rounded-full mb-10" />
+        <Skeleton className="h-8 w-8 rounded-full mb-10" />
+      </div>
     </div>
   );
 };
