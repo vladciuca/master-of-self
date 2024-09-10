8675a27220af02c8358bb52771eb3ddaf41f8f69
Vlad Ciuca <vladccdev@gmail.com>
2024-09-10 15:11:39 +0300

    adjusted habit step skeleton

--- components/skeletons/SkeletonHabitLevel.tsx
+++ components/skeletons/SkeletonHabitLevel.tsx
@@ -2,8 +2,18 @@ import { Skeleton } from "@components/ui/skeleton";
 
 const SkeletonHabitLevel = () => {
   return (
-    <div className="mx-12 mt-20">
-      <div className="flex justify-around items-center">
+    <div className="mx-12 mt-8">
+      <div className="flex justify-around items-center my-8">
+        <Skeleton className="h-8 w-8 rounded-full mb-10" />
+        <Skeleton className="h-24 w-24 rounded-full mb-10" />
+        <Skeleton className="h-8 w-8 rounded-full mb-10" />
+      </div>
+      <div className="flex justify-around items-center my-8">
+        <Skeleton className="h-8 w-8 rounded-full mb-10" />
+        <Skeleton className="h-24 w-24 rounded-full mb-10" />
+        <Skeleton className="h-8 w-8 rounded-full mb-10" />
+      </div>
+      <div className="flex justify-around items-center my-8">
         <Skeleton className="h-8 w-8 rounded-full mb-10" />
         <Skeleton className="h-24 w-24 rounded-full mb-10" />
         <Skeleton className="h-8 w-8 rounded-full mb-10" />
