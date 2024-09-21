2f8a26de2a520f2589c6c8149363175085fb8ba3
Vlad Ciuca <vladccdev@gmail.com>
2024-09-21 13:31:40 +0300

    prod test auth

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,45 +16,46 @@ type Providers = {
 
 export function Footer() {
   const { data: session, status } = useSession();
-  const [providers, setProviders] = useState<Providers | null>(null);
+  // const [providers, setProviders] = useState<Providers | null>(null);
 
-  useEffect(() => {
-    const setUpProviders = async () => {
-      const response: any = await getProviders();
-      setProviders(response as Providers);
-    };
+  // useEffect(() => {
+  //   const setUpProviders = async () => {
+  //     const response: any = await getProviders();
+  //     setProviders(response as Providers);
+  //   };
 
-    setUpProviders();
-  }, []);
+  //   setUpProviders();
+  // }, []);
 
-  const createSignInHandler = (providerId: string) => () =>
-    signIn(providerId, { callbackUrl: "/journal" });
+  // const createSignInHandler = (providerId: string) => () =>
+  //   signIn(providerId, { callbackUrl: "/journal" });
 
-  if (status === "loading" || !providers) {
-    return (
-      <div className="w-full h-full flex justify-center items-center">
-        <div className="loader" />
-      </div>
-    );
-  }
+  // if (status === "loading" || !providers) {
+  //   return (
+  //     <div className="w-full h-full flex justify-center items-center">
+  //       <div className="loader" />
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="w-full h-full flex justify-center items-center">
       {session?.user ? (
         <BottomNav />
       ) : (
-        providers &&
-        Object.values(providers).map((provider: ClientSafeProvider) => (
-          <Button
-            className="w-1/2"
-            type="button"
-            key={provider.name}
-            onClick={createSignInHandler(provider.id)}
-            // disabled={status === "loading"}
-          >
-            Sign In
-          </Button>
-        ))
+        // providers &&
+        // Object.values(providers).map((provider: ClientSafeProvider) => (
+        <Button
+          className="w-1/2"
+          type="button"
+          // key={provider.name}
+          // onClick={createSignInHandler(provider.id)}
+          onClick={() => signIn("google")}
+          disabled={status === "loading"}
+        >
+          Sign In
+        </Button>
+        // ))
       )}
     </div>
   );
