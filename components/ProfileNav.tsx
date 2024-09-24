95710faf5b41d79d0fd273106ab6b726dd02b39d
Vlad Ciuca <vladccdev@gmail.com>
2024-09-24 15:23:53 +0300

    added total willpower to stat

--- components/ProfileNav.tsx
+++ components/ProfileNav.tsx
@@ -2,6 +2,8 @@
 
 import Link from "next/link";
 import { useTotalWillpower } from "@hooks/useTotalWillpower";
+import { FaBoltLightning, FaLink, FaSun } from "react-icons/fa6";
+// import { Progress } from "@components/ui/progress";
 import { Session } from "@app/types/types";
 
 type ProfileBarProps = {
@@ -12,7 +14,6 @@ export function ProfileNav({ session }: ProfileBarProps) {
   const { totalWillpower, totalWillpowerLoading, totalWillpowerError } =
     useTotalWillpower();
   const name = session.user?.name || "";
-  // const email = session.user?.email || "";
   const nameInitials = name
     ? name
         .split(" ")
@@ -21,16 +22,41 @@ export function ProfileNav({ session }: ProfileBarProps) {
     : "";
 
   return (
-    <div className="flex items-center h-full">
-      <div className="w-1/4 flex justify-center mx-2">
+    <div className="flex items-center justify-between h-full mx-8">
+      <div className="flex justify-center mx-2">
         <Link href="/profile">
           <div className="avatar text-xl font-semibold bg-primary text-primary-foreground rounded-full h-12 w-12 flex justify-center items-center">
             {nameInitials}
           </div>
         </Link>
       </div>
-      <div className="flex grow">
-        {totalWillpowerLoading ? "??" : totalWillpower}
+
+      <div className="flex flex-col flex-grow ml-6">
+        <div>
+          <div className="flex justify-between items-baseline">
+            <span className="text-sm font-medium">Journal Streak</span>
+            <div className="flex items-center text-xl font-bold">
+              {1}
+              <div className="text-sm">
+                <FaSun className="ml-2 text-xl" />
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between items-baseline">
+            <span className="text-sm font-medium">Willpower Generated</span>
+            <div className="flex items-center text-xl font-bold">
+              {totalWillpowerLoading ? "??" : totalWillpower}
+              <div className="text-sm">
+                <FaBoltLightning className="ml-2 text-xl" />
+              </div>
+            </div>
+          </div>
+          {/* <Progress value={10} max={30} className="h-2 mt-1 mb-2" />
+          <div className="flex justify-between">
+            <span className="text-xs font-medium">Milestone 1 (7 days)</span>
+            <span className="text-xs font-medium">Evening 00:00</span>
+          </div> */}
+        </div>
       </div>
     </div>
   );
