8f767b56a168c0ae85a6ac60a3d3be14188b4156
Vlad Ciuca <vladccdev@gmail.com>
2024-10-03 18:37:57 +0300

    added checkbox to actions

--- components/journal/journal-entry-form/form-steps/HabitActions.tsx
+++ components/journal/journal-entry-form/form-steps/HabitActions.tsx
@@ -1,6 +1,8 @@
+import { act, useState } from "react";
 import { FormStepTemplate } from "@components/journal/journal-entry-form/form-steps/FormStepTemplate";
 import { HabitCardHeader } from "@components/habits/habit-card/HabitCardHeader";
 import { Checkbox } from "@components/ui/checkbox";
+import { Badge } from "@components/ui/badge";
 import { useUserHabits } from "@hooks/useUserHabits";
 import {
   CircleX,
@@ -10,6 +12,56 @@ import {
   Hash,
   Clock,
 } from "lucide-react";
+import { Action } from "@app/types/types";
+
+type HabitActionProps = {
+  action: Action;
+};
+
+export function HabitAction({ action }: HabitActionProps) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <div className="px-4 my-6">
+      <div className="text flex justify-between items-center border-b pb-1 mb-2">
+        <div className="flex items-center">
+          {action.type === "offensive" ? (
+            <CircleAlert className="mr-2 text-blue-500" size={24} />
+          ) : (
+            <OctagonAlert className="mr-2 text-blue-500" size={24} />
+          )}
+          {action.action}
+        </div>
+
+        <Checkbox
+          className="h-8 w-8 mr-4"
+          checked={isChecked}
+          onClick={() => setIsChecked(!isChecked)}
+        />
+      </div>
+
+      {isChecked && (
+        <div>
+          <div className="flex justify-around items-center">
+            <div>MINUS</div>
+            <div>
+              Value
+              <Badge variant="outline" className="capitalize">
+                {action.metric === "count" ? (
+                  <Hash size={18} className="mr-2" />
+                ) : (
+                  <Clock size={18} className="mr-2" />
+                )}
+                {action.metric}
+              </Badge>
+            </div>
+            <div>PLUS</div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
 
 export function HabitActions() {
   const { habits, habitsLoading, habitsError } = useUserHabits();
@@ -24,46 +76,51 @@ export function HabitActions() {
                 <div>
                   {habit.actions.map((action) => {
                     return (
-                      <div className="px-4 my-4">
-                        <div className="text flex justify-between items-center border-b pb-1 mb-2">
-                          <div className="flex items-center">
-                            {action.type === "offensive" ? (
-                              <CircleAlert
-                                className="mr-2 text-blue-500"
-                                size={20}
-                              />
-                            ) : (
-                              <OctagonAlert
-                                className="mr-2 text-blue-500"
-                                size={20}
-                              />
-                            )}
-                            {action.action}
-                          </div>
-                          {/* <button
-                            type="button"
-                            // onClick={() => removeAction(index)}
-                            className="ml-2 text-red-500"
-                            aria-label={`Remove action ${action.action}`}
-                          >
-                            <CircleX size={20} />
-                          </button> */}
-                          <Checkbox className="h-8 w-8" />
-                        </div>
-                        {/* <div className="flex items-center space-x-1">
-                          <Badge variant="secondary" className="capitalize">
-                            {action.type}
-                          </Badge>
-                          <Badge variant="outline" className="capitalize">
-                            {action.metric === "count" ? (
-                              <Hash size={18} className="mr-2" />
-                            ) : (
-                              <Clock size={18} className="mr-2" />
-                            )}
-                            {action.metric}
-                          </Badge>
-                        </div> */}
-                      </div>
+                      <HabitAction action={action} />
+                      // <div className="px-4 my-6">
+                      //   <div className="text flex justify-between items-center border-b pb-1 mb-2">
+                      //     <div className="flex items-center">
+                      //       {action.type === "offensive" ? (
+                      //         <CircleAlert
+                      //           className="mr-2 text-blue-500"
+                      //           size={24}
+                      //         />
+                      //       ) : (
+                      //         <OctagonAlert
+                      //           className="mr-2 text-blue-500"
+                      //           size={24}
+                      //         />
+                      //       )}
+                      //       {action.action}
+                      //     </div>
+
+                      //     <Checkbox
+                      //       className="h-8 w-8 mr-4"
+                      //       checked={isChecked}
+                      //       onClick={() => setIsChecked(!isChecked)}
+                      //     />
+                      //   </div>
+
+                      //   {isChecked && (
+                      //     <div>
+                      //       <div className="flex justify-around items-center">
+                      //         <div>MINUS</div>
+                      //         <div>
+                      //           Value
+                      //           <Badge variant="outline" className="capitalize">
+                      //             {action.metric === "count" ? (
+                      //               <Hash size={18} className="mr-2" />
+                      //             ) : (
+                      //               <Clock size={18} className="mr-2" />
+                      //             )}
+                      //             {action.metric}
+                      //           </Badge>
+                      //         </div>
+                      //         <div>PLUS</div>
+                      //       </div>
+                      //     </div>
+                      //   )}
+                      // </div>
                     );
                   })}
                 </div>
