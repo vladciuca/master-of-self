ff78d444ae561d8f6e1e7e9a213f95dd38dc804f
Vlad Ciuca <vladccdev@gmail.com>
2024-10-11 13:58:27 +0300

    styled habit card actions

--- components/habits/habit-card/HabitCardActions.tsx
+++ components/habits/habit-card/HabitCardActions.tsx
@@ -1,5 +1,5 @@
 import { ActionIcon } from "@components/habits/HabitActionFragments";
-import { Badge } from "@/components/ui/badge";
+// import { Badge } from "@/components/ui/badge";
 import { Hash, Clock } from "lucide-react";
 import { HabitAction } from "@app/types/types";
 
@@ -16,13 +16,13 @@ export function HabitCardActions({
     <div>
       {actions.map((action, index) => (
         <div key={index} className="mb-6">
-          <div className="flex items-center justify-between pb-2 mb-2 border-b">
+          <div className="flex items-center justify-between pb-2 mb-1 border-b">
             <div className="flex items-center">
               <span className="flex items-center">
                 <ActionIcon type={action.type} />
-                <Badge variant="secondary" className="mr-2">
+                {/* <Badge variant="secondary" className="mr-2">
                   {action.type === "offensive" ? "I will" : "I won't"}
-                </Badge>
+                </Badge> */}
               </span>
               <span className=" text-lg">{action.action}</span>
             </div>
@@ -31,33 +31,35 @@ export function HabitCardActions({
             {/* <Badge variant="secondary" className="capitalize">
               {action.type}
             </Badge> */}
-            <div className="flex items-center space-x-2">
-              <div className="text-xl flex items-center">
-                {action.metric === "count" ? (
-                  <Hash size={18} className="mr-1 text-primary" />
-                ) : (
-                  <Clock size={18} className="mr-1 text-primary" />
-                )}
-                <span className="font-bold flex items-center text-primary">
-                  {action.value}
-                  {actionUpdateValues[action.id] ? (
-                    <span className="text-green-500 ml-1">
-                      +{actionUpdateValues[action.id]}
+
+            <div className="flex items-baseline mx-1">
+              Overall:
+              <span className="text-primary ml-1">
+                <div className="flex items-baseline space-x-2">
+                  <div className="flex items-baseline ml-2">
+                    <span className="flex items-baseline">
+                      {action.metric === "count" ? (
+                        <Hash size={16} className="text-primary" />
+                      ) : (
+                        <Clock size={16} className="text-primary" />
+                      )}
                     </span>
-                  ) : (
-                    <></>
-                  )}
-                  {action.metric === "count" ? "" : " h"}
-                </span>
-              </div>
+
+                    <span className="ml-1 text-xl font-bold flex items-baseline text-primary">
+                      {action.value}
+                      {actionUpdateValues[action.id] ? (
+                        <span className="text-green-500 ml-1">
+                          +{actionUpdateValues[action.id]}
+                        </span>
+                      ) : (
+                        <></>
+                      )}
+                      {action.metric === "count" ? "" : " h"}
+                    </span>
+                  </div>
+                </div>
+              </span>
             </div>
-            <span className="text-green-500">
-              +
-              {actionUpdateValues[action.id]
-                ? action.value + actionUpdateValues[action.id]
-                : action.value}
-              <span className="text-primary mx-1">XP</span>
-            </span>
           </div>
         </div>
       ))}
