ccd539fd1076915792bde3c690844e7983b1c03e
Vlad Ciuca <vladccdev@gmail.com>
2024-10-12 18:49:03 +0300

    styled action stats on habit card

--- components/habits/habit-card/HabitCardActions.tsx
+++ components/habits/habit-card/HabitCardActions.tsx
@@ -18,18 +18,18 @@ export function HabitCardActions({
     <div>
       {actions.map((action, index) => (
         <div key={index} className="mb-6">
-          <div className="flex items-center justify-between pb-2 mb-1 border-b">
+          <div className="flex items-center justify-between mb-1">
             <div className="flex items-center">
               <span className="flex items-center">
                 <ActionIcon type={action.type} />
               </span>
-              <span className=" text-lg">{action.action}</span>
+              <span className="text-lg">{action.action}</span>
             </div>
           </div>
           <div className="flex flex-col text-sm text-muted-foreground mb-2">
-            <div className="flex items-baseline mx-1 my-1">
+            <div className="flex items-center justify-between border border-muted rounded-md p-2 my-1">
               Daily target:
-              <span className="ml-1 text-lg font-bold flex items-baseline text-primary">
+              <span className="ml-2 font-bold flex items-center text-primary">
                 {actionUpdateValues[action.id] ? (
                   <span className="text-green-500">
                     {actionUpdateValues[action.id]}
@@ -41,14 +41,15 @@ export function HabitCardActions({
               </span>
             </div>
 
-            <div className="flex items-baseline mx-1">
-              Overall:
-              <span className="text-primary ml-1">
-                <div className="flex items-baseline space-x-2">
-                  <div className="flex items-baseline ml-2">
+            <div className="flex items-center justify-between border border-muted rounded-md p-2 my-1">
+              <span>Overall</span>
+
+              <span className="text-primary ml-2">
+                <div className="flex items-center space-x-2">
+                  <div className="flex items-center">
                     <MetricIcon metric={action.metric} size={16} />
 
-                    <span className="ml-1 text-lg font-bold flex items-baseline text-primary">
+                    <span className="ml-1 font-bold flex items-baseline text-primary">
                       {formatNumberSuffixes(
                         action.value + (actionUpdateValues[action.id] || 0)
                       )}
