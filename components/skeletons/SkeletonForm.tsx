01e706a2306ec7357e3eab150f8769ea393ec443
Vlad Ciuca <vladccdev@gmail.com>
2024-10-12 18:23:05 +0300

    added skeleton habit form

--- components/skeletons/SkeletonForm.tsx
+++ components/skeletons/SkeletonForm.tsx
@@ -1,16 +1,21 @@
-import { Skeleton } from "@components/ui/skeleton";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export function SkeletonForm() {
   return (
-    <div className="w-full flex flex-col space-y-8 my-10 h-full">
-      <Skeleton className="w-1/3 h-[45px] mb-6" />
+    <div className="flex flex-col h-full">
+      <div className="flex flex-col items-center">
+        <Skeleton className="rounded-full h-[85px] w-[85px] mt-20" />
+      </div>
+      <div className="flex flex-grow flex-col justify-between">
+        <div className="flex flex-col items-center mt-10">
+          <Skeleton className="w-full h-[35px]" />
+          <Skeleton className="w-full h-[100px] mt-4" />
+        </div>
 
-      <Skeleton className="w-full h-[35px]" />
-      <Skeleton className="w-full h-[100px]" />
-
-      <div className="flex flex-col flex-grow justify-center items-center">
-        <Skeleton className="w-1/2 h-[42px] mt-6 mb-6" />
-        <Skeleton className="w-1/2 h-[42px]" />
+        <div className="flex flex-col items-center mt-auto">
+          <Skeleton className="w-full h-[42px]" />
+          <Skeleton className="w-full h-[42px] mt-4" />
+        </div>
       </div>
     </div>
   );
