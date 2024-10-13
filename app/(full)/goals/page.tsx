7e039a4d8ace0f0a7c2f4c3ceac7e713c63c0da6
Vlad Ciuca <vladccdev@gmail.com>
2024-10-13 15:24:10 +0300

    centerd content on goal page

--- app/(full)/goals/page.tsx
+++ app/(full)/goals/page.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
-import { PageHeader } from "@components/PageHeader";
-import { Button } from "@components/ui/button";
-import { Shell } from "lucide-react";
-import { Target } from "lucide-react";
+import { PageHeader } from "@/components/PageHeader";
+import { Button } from "@/components/ui/button";
+import { Shell, Target } from "lucide-react";
 
 const PAGE_HEADER_DETAILS = {
   symbol: <Target size={"2rem"} />,
@@ -12,7 +11,7 @@ const PAGE_HEADER_DETAILS = {
 
 export default function Goals() {
   return (
-    <div>
+    <div className="flex flex-col h-screen">
       <PageHeader
         symbol={PAGE_HEADER_DETAILS.symbol}
         title={PAGE_HEADER_DETAILS.title}
@@ -20,12 +19,10 @@ export default function Goals() {
         numberOfEntries={0}
         disabled={true}
       />
-      <div>
-        <section className="h-full flex flex-col items-center justify-center">
-          <h1 className="scroll-m-20 text-4xl font-bold text-center">
-            COMING SOON
-          </h1>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
+      <div className="flex-grow flex items-center justify-center mb-44">
+        <section className="text-center">
+          <h1 className="scroll-m-20 text-4xl font-bold">COMING SOON</h1>
+          <p className="leading-7 mt-6">
             Until then you can track your Habits!
           </p>
           <Link href="/habits">
