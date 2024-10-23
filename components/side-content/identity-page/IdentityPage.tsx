9a7c212c44e946820a2c0dedbaa60c31dd2f78e8
Vlad Ciuca <vladccdev@gmail.com>
2024-10-23 23:14:16 +0300

    cleaned out identity page

--- components/side-content/identity-page/IdentityPage.tsx
+++ components/side-content/identity-page/IdentityPage.tsx
@@ -54,7 +54,7 @@ function HeroSection({ isDrawerOpen }: { isDrawerOpen: boolean }) {
               animate={{ scale: 1, opacity: 1 }}
               transition={{ delay: 0.5, duration: 0.5 }}
             >
-              2.0 Self
+              2.0 <span className="text-primary">Self</span>
             </motion.span>
           </>
         )}
@@ -64,7 +64,6 @@ function HeroSection({ isDrawerOpen }: { isDrawerOpen: boolean }) {
 }
 
 type AnimatedSectionProps = {
-  type: string;
   subtitle: string;
   content: string;
   id: string;
@@ -72,7 +71,6 @@ type AnimatedSectionProps = {
 };
 
 function AnimatedSection({
-  type,
   subtitle,
   content,
   id,
@@ -85,34 +83,39 @@ function AnimatedSection({
 
   return (
     <AnimatePresence>
-      {(isDrawerOpen || inView) && (
+      {isDrawerOpen && (
         <motion.section
           id={id}
           ref={ref}
           className="py-12 bg-muted mx-12 mb-12 rounded-3xl"
           initial={{ opacity: 0, y: 50 }}
-          animate={inView && { opacity: 1, y: 0 }}
+          animate={isDrawerOpen && { opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 50 }} // Optional exit animation when the section leaves the view
           transition={{ duration: 0.8 }}
         >
           <div className="container mx-auto px-6">
-            {/* <h2 className="text-2xl font-semibold text-blue-500 mb-2">
-              {type}
-            </h2> */}
-
-            <motion.div
-              className={`h-20 w-20 rounded-full ${
-                id === "identity"
-                  ? "bg-blue-500"
-                  : id === "behavior"
-                  ? "bg-pink-500"
-                  : id === "believes"
-                  ? "bg-purple-500"
-                  : "bg-gray-500"
-              }`}
-            />
-            <h3 className="text-3xl font-bold mb-12 text-center">{subtitle}</h3>
-            <p className="text-lg leading-relaxed">{content}</p>
+            <h3 className="text-3xl font-bold text-center">{subtitle}</h3>
+            <div className="w-full flex justify-center my-12">
+              <motion.div
+                className={`h-20 w-20 rounded-full ${
+                  id === "identity"
+                    ? "bg-blue-500"
+                    : id === "behavior"
+                    ? "bg-pink-500"
+                    : id === "believes"
+                    ? "bg-purple-500"
+                    : "bg-gray-500"
+                }`}
+                initial="hidden"
+                animate="visible"
+                variants={{
+                  hidden: { opacity: 0, y: 20 },
+                  visible: { opacity: 1, y: 0 },
+                }}
+                transition={{ duration: 0.5 }}
+              />
+            </div>
+            <p className="text-lg leading-relaxed text-center">{content}</p>
           </div>
         </motion.section>
       )}
@@ -125,7 +128,6 @@ function IdentitySection({ isDrawerOpen }: { isDrawerOpen: boolean }) {
     <AnimatedSection
       isDrawerOpen={isDrawerOpen}
       id="identity"
-      type="identity"
       subtitle="Visualize Your Ideal Self"
       content="Imagine your most empowered version. Describe the appearance of your 2.0 self in vivid detail, from accessories to clothing and style, let your creativity shine as you craft the best version of you. We'll bring it to life through a custom avatar that reflects your ideal vision."
     />
@@ -137,8 +139,7 @@ function BelievesSection({ isDrawerOpen }: { isDrawerOpen: boolean }) {
     <AnimatedSection
       isDrawerOpen={isDrawerOpen}
       id="believes"
-      type="BELIEVES"
-      subtitle="Describe Your Ideal Self's Achievements"
+      subtitle="What are your Achievements?"
       content="Envision your ideal self's accomplishments and write them down as powerful affirmations in the present tense: 'I earn...', 'I make...', 'I drive...'. By framing your goals this way, you tap into the mindset of success. Select one goal, break it down into actionable steps, and map out your hero's journey by setting clear milestones along the way. Each milestone represents progress, and with each step, you're closer to becoming your 2.0 self. Build habits with actions that align to these milestones, ensuring steady growth towards your ultimate achievements."
     />
   );
@@ -149,8 +150,7 @@ function BehaviorSection({ isDrawerOpen }: { isDrawerOpen: boolean }) {
     <AnimatedSection
       isDrawerOpen={isDrawerOpen}
       id="behavior"
-      type="BEHAVIOR"
-      subtitle="Define Key Habits"
+      subtitle="What are your Key Habits?"
       content="Success is built on consistent actions. To bridge the gap between who you are now and your 2.0 self, identify the essential habits that will drive your transformation. These are the daily or weekly actions that, when repeated, lead to achieving your bigger goals. Whether it's developing a morning routine, staying focused on deep work, or committing to lifelong learning, these habits will drive your progress and help you align with the version of yourself you're striving to become."
     />
   );
