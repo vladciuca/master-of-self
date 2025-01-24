ec22f69e4ae893989be73f9919c4e9fad46f8de3
Vlad Ciuca <vladccdev@gmail.com>
2025-01-24 22:24:02 +0200

    adjusted sign in element size and added temp test logo

--- components/PageLogo.tsx
+++ components/PageLogo.tsx
@@ -1,18 +1,18 @@
 import {
   GiAllSeeingEye,
-  GiSuspicious,
-  GiMeditation,
-  GiLotus,
-  GiMonkFace,
-  GiThreeLeaves,
-  GiYinYang,
-  GiThirdEye,
-  GiEvilBook,
-  GiMagicGate,
-  GiCrystalBall,
-  GiMagicSwirl,
-  GiEyeOfHorus,
-  GiLouvrePyramid,
+  // GiSuspicious,
+  // GiMeditation,
+  // GiLotus,
+  // GiMonkFace,
+  // GiThreeLeaves,
+  // GiYinYang,
+  // GiThirdEye,
+  // GiEvilBook,
+  // GiMagicGate,
+  // GiCrystalBall,
+  // GiMagicSwirl,
+  // GiEyeOfHorus,
+  // GiLouvrePyramid,
 } from "react-icons/gi";
 
 export function PageLogo() {
@@ -22,3 +22,13 @@ export function PageLogo() {
     </div>
   );
 }
+
+// import Image from "next/image";
+
+// export function PageLogo() {
+//   return (
+//     <div className="h-full flex items-center justify-center">
+//       <Image src="/logo.svg" alt="Logo" width={320} height={320} />
+//     </div>
+//   );
+// }
--- components/ProviderSignIn.tsx
+++ components/ProviderSignIn.tsx
@@ -21,7 +21,7 @@ type ProviderStyles = {
 const providerStyles: ProviderStyles = {
   google: {
     icon: (
-      <svg viewBox="0 0 24 24" className="mr-3 h-8 w-8">
+      <svg viewBox="0 0 24 24" className="mr-3 h-6 w-6">
         <path
           fill="#4285F4"
           d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
@@ -68,7 +68,7 @@ const providerStyles: ProviderStyles = {
   //   },
   github: {
     icon: (
-      <svg viewBox="0 0 24 24" className="mr-3 h-8 w-8">
+      <svg viewBox="0 0 24 24" className="mr-3 h-6 w-6">
         <path
           fill="#24292F"
           d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z"
@@ -144,8 +144,8 @@ export function ProviderSignIn() {
 
   return (
     <div className="w-full h-full flex flex-col justify-around p-6 space-y-4">
-      <div className="w-full flex flex-col items-center justify-center">
-        <GiSecretBook size={100} />
+      <div className="w-full flex flex-col items-center justify-center mt-8">
+        <GiSecretBook size={70} />
         {/* <h2 className="mt-2 scroll-m-20 text-xl font-semibold transition-colors first:mt-0">
           Sign In
         </h2> */}
@@ -158,10 +158,10 @@ export function ProviderSignIn() {
               onClick={createSignInHandler(providerId)}
               disabled={isLoading || !providers?.[providerId]}
               variant="default"
-              className={`w-full h-14 text-md font-medium justify-between items-center ${style.className}`}
+              className={`w-full h-10 text-base font-medium justify-between items-center ${style.className}`}
             >
               {isLoading ? (
-                <Loader2 className="h-8 w-8 animate-spin" />
+                <Loader2 className="h-6 w-6 animate-spin" />
               ) : (
                 style.icon
               )}
