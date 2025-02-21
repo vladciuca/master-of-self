d11e37265debba02a48ec528002cc03e55f41710
Vlad Ciuca <vladccdev@gmail.com>
2025-02-21 07:43:12 +0200

    fix ios safari button focus issue

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -45,6 +45,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
+        style={{ outline: "none", userSelect: "none" }}
         ref={ref}
         {...props}
       />
