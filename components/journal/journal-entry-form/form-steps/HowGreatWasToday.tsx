c5f97e77da3c4bfc3d0dfbb9af5ffab3f05db3e1
Vlad Ciuca <vladccdev@gmail.com>
2025-02-27 14:48:22 +0200

    updated UI for HowGreatWasToday

--- components/journal/journal-entry-form/form-steps/HowGreatWasToday.tsx
+++ components/journal/journal-entry-form/form-steps/HowGreatWasToday.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { FormStepTemplate } from "@components/journal/journal-entry-form/form-steps/FormStepTemplate";
+import { Badge } from "@components/ui/badge";
 import { Checkbox } from "@components/ui/checkbox";
 import { useState, useEffect } from "react";
 
@@ -47,27 +48,40 @@ export function HowGreatWasToday({
     >
       <ol className="list-decimal py-2 mt-2 mx-4 space-y-3">
         {greatToday.map((item, index) => (
-          <li key={index} className="flex items-top justify-between">
-            <div className="flex items-top text-base">
-              <div className={`${checkedItems[item] ? "text-green-500" : ""}`}>
-                {index + 1}.
+          <li key={index} className="flex justify-between w-full">
+            <div className="inline-flex text-base w-5/6">
+              <div className="w-1/6">
+                <Badge
+                  className={`h-[30px] w-[30px] text-base text-primary bg-transparent hover:bg-transparent ${
+                    checkedItems[item]
+                      ? "bg-[linear-gradient(to_right,_#eab308_50%,_#a855f7_50%)] bg-origin-border"
+                      : ""
+                  }`}
+                >
+                  <div className="h-full w-full flex justify-center items-center">
+                    {index + 1}.
+                  </div>
+                </Badge>
               </div>
+
               <span
                 // className={`ml-1 mr-2 ${
                 //   checkedItems[item] ? "line-through" : ""
                 // }`}
-                className="ml-1 mr-2"
+                className="break-words w-5/6 text-left mt-1"
               >
                 {item}
               </span>
             </div>
 
-            <Checkbox
-              id={`checkbox-${index}`}
-              className="h-8 w-8"
-              checked={checkedItems[item] || false}
-              onCheckedChange={() => handleCheckboxChange(item)}
-            />
+            <div className="flex justify-end w-1/6">
+              <Checkbox
+                id={`checkbox-${index}`}
+                className="h-8 w-8"
+                checked={checkedItems[item] || false}
+                onCheckedChange={() => handleCheckboxChange(item)}
+              />
+            </div>
           </li>
         ))}
       </ol>
