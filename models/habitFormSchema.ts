50c962c312d161ea5a3fb589e768ff12d0adb55d
Vlad Ciuca <vladccdev@gmail.com>
2025-03-05 20:07:41 +0200

    updated error descriptions

--- models/habitFormSchema.ts
+++ models/habitFormSchema.ts
@@ -14,9 +14,9 @@ export const habitFormSchema = z.object({
   id: z.string().optional(),
   category: z
     .string()
-    .min(3, "Habit category name must contain at least 3 characters")
-    .max(25, "Habit category name must contain maximum 25 characters"),
-  icon: z.string().min(1, "Please select a category icon"),
+    .min(3, "Habit name must contain at least 3 characters")
+    .max(25, "Habit name must contain maximum 25 characters"),
+  icon: z.string().min(1, "Please select a icon"),
   xp: z.number().optional(),
   actions: z.array(actionSchema).min(1, "Please add a habit action"),
 });
