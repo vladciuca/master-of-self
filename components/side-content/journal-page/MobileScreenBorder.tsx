b6bbbbabcdfa41cfb490239c9be69ace09e797a5
Vlad Ciuca <vladccdev@gmail.com>
2025-03-28 10:08:00 +0200

    adjusted mobile frame width

--- components/side-content/journal-page/MobileScreenBorder.tsx
+++ components/side-content/journal-page/MobileScreenBorder.tsx
@@ -23,7 +23,7 @@ MobileScreenBorderProps) {
   const content = children;
   //   || img
   return (
-    <div className="my-6 h-[522px] w-[300px] relative bg-background sm:border-4 sm:border-muted sm:rounded-3xl sm:shadow-xl sm:shadow-gray-950 mx-auto flex flex-col items-center justify-center max-w-[450px] sm:max-h-[800px] overflow-hidden">
+    <div className="my-6 h-[522px] w-[295px] relative bg-background sm:border-4 sm:border-muted sm:rounded-3xl sm:shadow-xl sm:shadow-gray-950 mx-auto flex flex-col items-center justify-center max-w-[450px] sm:max-h-[800px] overflow-hidden">
       {/* Notch */}
       <div className="hidden md:block bg-muted absolute left-1/2 top-0 h-2 w-20 -translate-x-1/2 rounded-b-3xl z-10"></div>
 
