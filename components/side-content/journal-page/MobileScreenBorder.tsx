53d2e1da6abd073e48c81e11181b6b9e63da177d
Vlad Ciuca <vladccdev@gmail.com>
2025-03-28 10:01:50 +0200

    added z-index for the notch on mobile frame for images

--- components/side-content/journal-page/MobileScreenBorder.tsx
+++ components/side-content/journal-page/MobileScreenBorder.tsx
@@ -23,9 +23,9 @@ MobileScreenBorderProps) {
   const content = children;
   //   || img
   return (
-    <div className="relative my-6 h-[522px] w-[300px] bg-background sm:border-4 sm:border-muted sm:rounded-3xl sm:shadow-xl sm:shadow-gray-950 mx-auto flex flex-col items-center justify-center max-w-[450px] sm:max-h-[800px] overflow-hidden">
+    <div className="my-6 h-[522px] w-[300px] relative bg-background sm:border-4 sm:border-muted sm:rounded-3xl sm:shadow-xl sm:shadow-gray-950 mx-auto flex flex-col items-center justify-center max-w-[450px] sm:max-h-[800px] overflow-hidden">
       {/* Notch */}
-      <div className="hidden md:block bg-muted absolute left-1/2 top-0 h-2 w-20 -translate-x-1/2 rounded-b-3xl"></div>
+      <div className="hidden md:block bg-muted absolute left-1/2 top-0 h-2 w-20 -translate-x-1/2 rounded-b-3xl z-10"></div>
 
       {/* Content */}
       <div className="relative w-full h-full">{content}</div>
