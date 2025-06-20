c02a7c0655b5a41eef5aca56e5bf2306ac2a29c1
Vlad Ciuca <vladccdev@gmail.com>
2025-06-20 11:05:47 +0300

    added motivation copy

--- components/disciplines/DisciplinesList.tsx
+++ components/disciplines/DisciplinesList.tsx
@@ -1,6 +1,5 @@
 import { DisciplineSectionDelimiter } from "@components/disciplines/DisciplineSectionDelimiter";
 import { DisciplineCardHeader } from "@components/disciplines/discipline-card/DisciplineCardHeader";
-import { DisciplineCardContent } from "@components/disciplines/discipline-card/DisciplineCardContent";
 import {
   Accordion,
   AccordionContent,
@@ -12,7 +11,11 @@ import { DisciplineCard } from "./discipline-card/DisciplineCard";
 import { Discipline } from "@models/mongodb";
 import type { JournalCustomStep, JournalCustomStepConfig } from "@models/types";
 
+import { getJournalStepStyle } from "@components/ui/constants";
+
 import { TbChevronCompactDown } from "react-icons/tb";
+import { JOURNAL_COLORS } from "@lib/colors";
+
 type DisciplinesListProps = {
   disciplineList: JournalCustomStepConfig[] | Discipline[];
   activeDisciplineList: JournalCustomStep[] | Discipline[];
@@ -105,7 +108,40 @@ export function DisciplinesList({
           </div>
         </AccordionTrigger>
         <AccordionContent>
-          <DisciplineCardContent title={"title"} description={"description"} />
+          <div className="mt-2 px-1">
+            <div className="mb-4">
+              Motivation is the base discipline that can be engaged during both
+              morning and evening.
+            </div>
+
+            <div>
+              <div className="mb-4">
+                It is composed of 3 daily steps, allowing you to plan your day
+                and reflect on the outcome.
+              </div>
+
+              <ul className="mb-4 ml-2">
+                <li className="flex items-center">
+                  <div
+                    className={`mr-2 bg-${JOURNAL_COLORS.day} w-2 h-2 rounded-full`}
+                  />
+                  What will make today great?
+                </li>
+                <li className="flex items-center">
+                  <div
+                    className={`mr-2 bg-[linear-gradient(to_right,_#eab308_50%,_#a855f7_50%)] w-2 h-2 rounded-full`}
+                  />
+                  What made today great?
+                </li>
+                <li className="flex items-center">
+                  <div
+                    className={`mr-2 bg-${JOURNAL_COLORS.night} w-2 h-2 rounded-full`}
+                  />
+                  Daily Highlights
+                </li>
+              </ul>
+            </div>
+          </div>
         </AccordionContent>
       </AccordionItem>
 
