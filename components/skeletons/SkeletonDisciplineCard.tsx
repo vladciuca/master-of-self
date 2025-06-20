8ddc923017712d4f668818175294661f8df22cac
Vlad Ciuca <vladccdev@gmail.com>
2025-06-20 12:11:00 +0300

    adjusted skeletons

--- components/skeletons/SkeletonDisciplineCard.tsx
+++ components/skeletons/SkeletonDisciplineCard.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@components/ui/skeleton";
 
 export function SkeletonDisciplineCard() {
   return (
-    <Card className="flex flex-col items-start w-full p-4">
+    <Card className="flex flex-col items-start w-full p-4 border-none">
       <Skeleton className="h-5 w-24 mb-4 mt-2" />
       <Skeleton className="h-4 w-full rounded-full mb-2" />
     </Card>
--- components/skeletons/SkeletonHabitCard.tsx
+++ components/skeletons/SkeletonHabitCard.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@components/ui/skeleton";
 
 export function SkeletonHabitCard() {
   return (
-    <Card className="mb-4 mt-1 p-4">
+    <Card className="mb-4 mt-1 p-4 border-none">
       <div className="flex justify-between items-center">
         <Skeleton className="w-1/2 h-8" />
         <Skeleton className="rounded-full h-16 w-16" />
