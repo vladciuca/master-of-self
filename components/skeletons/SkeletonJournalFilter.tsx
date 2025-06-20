e4bc4d4b1caf1ff12af7abd146b56b0ec992be4a
Vlad Ciuca <vladccdev@gmail.com>
2025-06-20 11:18:12 +0300

    adjusted journal page skeleton

--- components/skeletons/SkeletonJournalFilter.tsx
+++ components/skeletons/SkeletonJournalFilter.tsx
@@ -2,8 +2,8 @@ import { Skeleton } from "@components/ui/skeleton";
 
 export function SkeletonJournalFilter() {
   return (
-    <div className="py-6">
-      <Skeleton className="rounded-full w-full h-4" />
+    <div className="py-4">
+      <Skeleton className="rounded-full w-full h-6" />
     </div>
   );
 }
