a912fa0ae89d805f4042600c602f9092e50efe4e
Vlad Ciuca <vladccdev@gmail.com>
2025-06-22 16:21:34 +0300

    adjusted habit card skeletons

--- components/skeletons/SkeletonHabitCard.tsx
+++ components/skeletons/SkeletonHabitCard.tsx
@@ -3,9 +3,14 @@ import { Skeleton } from "@components/ui/skeleton";
 
 export function SkeletonHabitCard() {
   return (
-    <Card className="mb-4 mt-1 p-4 border-none">
+    <Card className="mt-1 mb-12 px-1 border-none">
       <div className="flex justify-between items-center">
-        <Skeleton className="w-1/2 h-8" />
+        <Skeleton className="w-16 h-16" />
+        <div className="flex flex-grow flex-col ml-6">
+          <Skeleton className="rounded-md h-4 w-1/6 mb-4" />
+          <Skeleton className="rounded-md h-4 w-1/2" />
+        </div>
+
         <Skeleton className="rounded-full h-16 w-16" />
       </div>
     </Card>
