d18258ec3a9efb312598193a6148efd2c4a788d5
Vlad Ciuca <vladccdev@gmail.com>
2025-06-22 14:58:46 +0300

    updated landing assets

--- components/journal/journal-entry-card/JournalEntryHabits.tsx
+++ components/journal/journal-entry-card/JournalEntryHabits.tsx
@@ -23,45 +23,7 @@ export function JournalEntryHabits({
   const { journalHabits, habitData } = useHabitsIcons(habitsXp);
 
   return (
-    // <div className="flex items-center flex-wrap mt-4">
-    //   {Object.entries(journalHabits).map(([id, value]) => {
-    //     const currentXp = habits[id]?.currentXp || 0;
-
-    //     // Only render the habit if its value is not 0
-    //     if (value !== 0) {
-    //       return (
-    //         <div key={id} className="flex items-center mr-3 my-1 flex-1 mb-2">
-    //           <div className="text-xl">
-    //             {habitData[id] ? (
-    //               <IconRenderer
-    //                 iconName={habitData[id].icon}
-    //                 xp={currentXp + value}
-    //                 className={`h-[${size}px] w-[${size}px] p-[2px] rounded-md`}
-    //                 size={size}
-    //               />
-    //             ) : (
-    //               <Skeleton className="h-6 w-6 rounded-md" />
-    //             )}
-    //           </div>
-    //           <div
-    //             className={`flex items-center text-primary ${
-    //               size > 25 ? "text-md" : "text-sm"
-    //             }`}
-    //           >
-    //             <span className="ml-2">
-    //               <XpDisplay xpValue={value} />
-    //               <span className="text-primary ml-1">XP</span>
-    //             </span>
-    //           </div>
-    //         </div>
-    //       );
-    //     }
-
-    //     // Return null for habits with value 0
-    //     return null;
-    //   })}
-    // </div>
-    <div className="grid grid-cols-3 gap-x-10 gap-y-4 mt-4 justify-items-center">
+    <div className="grid grid-cols-3 gap-x-10 gap-y-4 justify-items-center my-2">
       {Object.entries(journalHabits).map(([id, value]) => {
         const currentXp = habits[id]?.currentXp || 0;
 
